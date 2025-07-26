1ce8dd44f23415ac54caf526f791523a8f5aa946
Gi. Osório <gi.osoorioh@gmail.com>
2025-07-26 13:59:38 -0300

    Atualizar o page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ const CRYPTO_CONFIG = [
     id: "wemix",
     name: "WEMIX",
     symbol: "WEMIX",
-    amount: 23.00,
+    amount: 22.00,
     displayAmount: "26,39",
     coinCapId: "wemix-token",
     cryptoCompareSymbol: "WEMIX",
